4752e8627e63898442415feaa79293838713d20d
ghazal-aroosa <65502534+ghazal-aroosa@users.noreply.github.com>
2020-06-03 11:22:48 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br>Updated by Ghazal Aroosa</br>
         </p>
         <a
           className="App-link"
